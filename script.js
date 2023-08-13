375b787d15ade0c0e2383c0007eb86ea6efa89c9
gaikwadbharati <127495861+gaikwadbharati@users.noreply.github.com>
2023-08-13 11:08:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let searchCont = document.querySelector('.search-cont');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchCont.classList.toggle('active')
+    navBar.classList.remove('show-nav')
+    shooPing.classList.remove('active-2')
+    user.classList.remove('active-3')
+
+}
+
+
+
+let shooPing = document.querySelector('.shooping');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shooPing.classList.toggle('active-2')
+    searchCont.classList.remove('active')
+    user.classList.remove('active-3')
+    navBar.classList.remove('show-nav')
+}
+
+
+let user = document.querySelector('.user');
+
+document.querySelector('#login-btn').onclick = () =>{
+    user.classList.toggle('active-3')
+    searchCont.classList.remove('active')
+    shooPing.classList.remove('active-2')
+    navBar.classList.remove('show-nav')
+  
+}
+
+window.onscroll = () => {
+    searchCont.classList.remove('active')
+    shooPing.classList.remove('active-2')
+    user.classList.remove('active-3')
+    navBar.classList.remove('show-nav')
+
+}
+
+let navBar = document.querySelector('.navlinks ');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navBar.classList.toggle('show-nav')
+    searchCont.classList.remove('active')
+    shooPing.classList.remove('active-2')
+    user.classList.remove('active-3')
+
+}
+
+
+// nav shadow
+
+let navCont = document.getElementsByClassName('nav-container')
+
+window.addEventListener('scroll' , () => {
+ navCont.classList.toggle("show-nav-shadow" , window.scrollY > 0);
+
+})
